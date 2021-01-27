202c5c6a656df2591545efc358ba73739f9998d8
Noe <noedelacroix@gmail.com>
2021-01-27 21:51:26 +0100

    Initial Commit

---
+++ Shared/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Shared
+//
+//  Created by Noe De La Croix on 27/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct HydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
